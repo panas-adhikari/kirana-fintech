99657872f6d11b67ab123eff6c08f27e7aacd35e
adhikaripanas@gmail.com <panas.a@skillrank.io>
2026-02-21 04:37:55 +0000

    Middleware fix PROD

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,7 +11,7 @@ export async function middleware(request: NextRequest) {
     // createServerClient from @supabase/ssr handles all the cookie logic for us
     const supabase = createServerClient(
         process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON!,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
         {
             cookies: {
                 getAll() {
